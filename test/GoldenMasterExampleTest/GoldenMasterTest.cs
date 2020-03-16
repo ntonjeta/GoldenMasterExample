d4bd43363d80ba97be7ebc2c83340372423f6086
ntonjeta <ntonjeta@autistici.org>
2020-03-16 18:13:00 +0100

    Given a tris legacy example

---
+++ test/GoldenMasterExampleTest/GoldenMasterTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace GoldenMasterExampleTest
+{
+    public class GoldenMasterTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FakeTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tris-app/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tris;
+using System;
+
+namespace tris_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.run();
+        }
+    }
+}
