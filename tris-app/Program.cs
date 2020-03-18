77173c4ac0be5df47e49100f89e138022858afa4
ntonjeta <ntonjeta@autistici.org>
2020-03-18 21:07:00 +0100

    And now redirect the stdout to file.

--- tris-app/Program.cs
+++ tris-app/Program.cs
@@ -7,13 +7,17 @@ namespace tris_app
     class Program
     {
         private const string InputPath = "input.txt";
+        private const string OutputPath = "output.txt";
 
         public static void Main(string[] args)
         {
             var input = new StreamReader(new FileStream(InputPath, FileMode.Open));
+            var output = new StreamWriter(new FileStream(OutputPath, FileMode.Create));
             Console.SetIn(input);
+            Console.SetOut(output);
             Game.run();
             input.Close();
+            output.Close();
         }
     }
 }
