cd669616a98c32a1287901b35506faedf8f4a96f
ntonjeta <ntonjeta@autistici.org>
2020-03-18 21:48:49 +0100

    Ok, now we have multiple output file. It's time to get interessing case.

--- tris-app/Program.cs
+++ tris-app/Program.cs
@@ -6,19 +6,21 @@ namespace tris_app
 {
     class Program
     {
-        private const string InputFolderPath = "input";
-        private const string OutputPath = "output/output.txt";
+        private const string InputFolderPath = "input/";
+        private const string OutputFolderPath = "output/";
 
         public static void Main(string[] args)
         {
+            int i = 1;
             foreach (var filePath in Directory.GetFiles(InputFolderPath)) {
                 var input = new StreamReader(new FileStream(filePath, FileMode.Open));
-                var output = new StreamWriter(new FileStream(OutputPath, FileMode.Append));
+                var output = new StreamWriter(new FileStream(OutputFolderPath + "output" + i.ToString() + ".txt" , FileMode.Append));
                 Console.SetIn(input);
                 Console.SetOut(output);
                 Game.run();
                 input.Close();
                 output.Close();
+                i++;
             }
         }
     }
