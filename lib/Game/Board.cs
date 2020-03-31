2fdf5a204343383d206746c53ed938b062670305
ntonjeta <ntonjeta@autistici.org>
2020-03-31 20:28:04 +0200

    Add board and test.

---
+++ lib/Game/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tris
+{
+    public class Board
+    {
+        private const int PlayerTwoId = 0;
+        private const int PlayerOneId = 1;
+
+        public double GetBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/GoldenMasterExampleTest/BoardShould.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Tris;
+
+namespace GoldenMasterExampleTest
+{
+    public class BoardShould
+    {
+        private Board _board;
+
+        private const int PlayerOne = 1;
+        private const int PlayerTwo = 0;
+        private const int PlayerOneChoice = 1;
+        private const int PlayerTwoChoice = 2;
+        private readonly char[] InitialBoard = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private readonly char[] UpdatedBoard = { 'X', 'O', '3', '4', '5', '6', '7', '8', '9' };
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new Board();
+        }
+
+        [Test]
+        public void ReturnBoard()
+        {
+            Assert.AreEqual(InitialBoard, _board.GetBoard());
+        }
+    }
+}
